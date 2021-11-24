d2fdeab4a68c3e6a318394946826889524757ce7
franjuh <90212872+franjuh@users.noreply.github.com>
2021-11-24 14:15:28 -0500

    Actualización src.js

--- src.js
+++ src.js
@@ -1,5 +1,6 @@
+import CITAS from 'global'
 //Array CITAS que simula el archivo en el ambiente de pruebas
-
+/* 
 CITAS = [
     {
         especialidad:"odontologia",
@@ -10,7 +11,25 @@ CITAS = [
     {
         especialidad:"medicina",
         nombre:"Dr. Mario",
-        fecha:"2020-01-01",
+        fecha:"2020-02-11",
+        hora:"09:00",
+    },
+    {
+        especialidad:"medicina",
+        nombre:"Dr. Mario",
+        fecha:"2021-05-01",
+        hora:"15:00",
+    },
+    {
+        especialidad:"medicina",
+        nombre:"Dr. Mario",
+        fecha:"2021-03-15",
+        hora:"15:00",
+    },
+    {
+        especialidad:"medicina",
+        nombre:"Dr. Mario",
+        fecha:"2015-01-01",
         hora:"15:00",
     },
     {
@@ -19,8 +38,8 @@ CITAS = [
         fecha:"2020-01-01",
         hora:"16:00",
     }
-]
-console.info(CITAS);
+] */
+//console.info(CITAS);
 
    
     const obtenerCitasDisponibles = (especialidad,fecha_inicio, fecha_final) =>{
@@ -29,24 +48,35 @@ console.info(CITAS);
     return CITAS.filter(cita => cita.especialidad==especialidad & Date.parse(cita.fecha)>=formatFechaInicio & Date.parse(cita.fecha)<=formatFechaFinal);
     }
 
-console.info(obtenerCitasDisponibles("optometria", "2019-12-31", "2020-02-12"));
+//console.info(obtenerCitasDisponibles("medicina", "2014-12-31", "2021-07-07"));
 
 
+    const obtenerCitasPorJornada=(especialidad, fecha_inicio, fecha_final, jornadaPreferida)=>{
+                        
+        const horaInicio = new Date();
+        const horaFin = new Date();
 
-    const obtenerCitasPorJornada=(especialidad,fecha_inicio, fecha_final, jornadaPreferida)=>{
-        if (jornadaPreferida = "mañana") { 
-            horaInicio="08:00";
-            horaFin="12:00";
-        } else if (jornadaPreferida = "tarde"){
-            horaInicio="13:00";
-            horaFin="17:00";
+        if (jornadaPreferida === "mañana") { 
+            horaInicio.setHours(08,00);
+            horaFin.setHours(12,00);
+        } else if (jornadaPreferida === "tarde"){
+            horaInicio.setHours(13,00);
+            horaFin.setHours(17,00);
         }
-        let resultados = obtenerCitasDisponibles (especialidad,fecha_inicio, fecha_final);
-        return resultados.filter(citaFinal => citaFinal.hora>=horaInicio & citaFinal.hora<=horaFin)
+        
+        let resultados = obtenerCitasDisponibles (especialidad, fecha_inicio, fecha_final);
+
+        return resultados.filter(function (resultado) {
+            let horaDiv = resultado.hora.split(":");
+            const citaFinal = new Date;
+            citaFinal2 = citaFinal.setHours(horaDiv[0],horaDiv[1]);
+            console.log (citaFinal2);
+            return citaFinal2 >= horaInicio & citaFinal2 <= horaFin;
+        });      
     }
 
-console.info(obtenerCitasPorJornada("optometria", "2019-12-31", "2020-02-12", "tarde"));
+//console.info(obtenerCitasPorJornada("medicina", "2019-12-31", "2020-02-12", "mañana"));
 
 
-// module.exports.obtenerCitasDisponibles=obtenerCitasDisponibles;
-// module.exports.obtenerCitasPorJornada=obtenerCitasPorJornada;
+ module.exports.obtenerCitasDisponibles=obtenerCitasDisponibles;
+ module.exports.obtenerCitasPorJornada=obtenerCitasPorJornada;
