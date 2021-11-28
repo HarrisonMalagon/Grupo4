ef24d2fd0eb9498f2db63c5f15a44f3261efbb39
franjuh <franjuh@yahoo.es>
2021-11-28 18:23:07 -0500

    Actualización Sprint3

---
+++ src(sprint2).js
@@ -0,0 +1,81 @@
+
+//Array CITAS que simula el archivo en el ambiente de pruebas
+/*  
+CITAS = [
+    {
+        especialidad:"odontologia",
+        nombre:"Dr. Juan",
+        fecha:"2020-01-01",
+        hora:"08:00",
+    },
+    {
+        especialidad:"medicina",
+        nombre:"Dr. Mario",
+        fecha:"2020-02-11",
+        hora:"09:00",
+    },
+    {
+        especialidad:"medicina",
+        nombre:"Dr. Mario",
+        fecha:"2021-05-01",
+        hora:"15:00",
+    },
+    {
+        especialidad:"medicina",
+        nombre:"Dr. Mario",
+        fecha:"2021-03-15",
+        hora:"15:00",
+    },
+    {
+        especialidad:"medicina",
+        nombre:"Dr. Mario",
+        fecha:"2015-01-01",
+        hora:"15:00",
+    },
+    {
+        especialidad:"optometria",
+        nombre:"Dr. Mario",
+        fecha:"2020-01-01",
+        hora:"16:00",
+    }
+]  */
+//console.info(CITAS);
+
+   
+    const obtenerCitasDisponibles = (especialidad,fecha_inicio, fecha_final) =>{
+    formatFechaInicio = Date.parse(fecha_inicio);
+    formatFechaFinal = Date.parse(fecha_final);
+    return CITAS.filter(cita => cita.especialidad==especialidad & Date.parse(cita.fecha)>=formatFechaInicio & Date.parse(cita.fecha)<=formatFechaFinal);
+    }
+
+//console.info(obtenerCitasDisponibles("medicina", "2014-12-31", "2021-07-07"));
+
+
+    const obtenerCitasPorJornada=(especialidad, fecha_inicio, fecha_final, jornadaPreferida)=>{
+                        
+        const horaInicio = new Date();
+        const horaFin = new Date();
+
+        if (jornadaPreferida === "mañana") { 
+            horaInicio.setHours("08","00");
+            horaFin.setHours("12","00");
+        } else if (jornadaPreferida === "tarde"){
+            horaInicio.setHours("13","00");
+            horaFin.setHours("17","00");
+        }
+        
+        let resultados = obtenerCitasDisponibles (especialidad, fecha_inicio, fecha_final);
+
+        return resultados.filter(function (resultado) {
+            let horaDiv = resultado.hora.split(":");
+            const citaFinal = new Date;
+            citaFinal2 = citaFinal.setHours(horaDiv[0],horaDiv[1]);
+            return citaFinal2 >= horaInicio & citaFinal2 <= horaFin;
+        });      
+    }
+
+//console.info(obtenerCitasPorJornada("medicina", "2019-12-31", "2020-02-12", "mañana"));
+
+
+module.exports.obtenerCitasDisponibles=obtenerCitasDisponibles;
+module.exports.obtenerCitasPorJornada=obtenerCitasPorJornada;
--- src.js
+++ src.js
@@ -1,81 +1,15 @@
 
-//Array CITAS que simula el archivo en el ambiente de pruebas
-/*  
-CITAS = [
-    {
-        especialidad:"odontologia",
-        nombre:"Dr. Juan",
-        fecha:"2020-01-01",
-        hora:"08:00",
-    },
-    {
-        especialidad:"medicina",
-        nombre:"Dr. Mario",
-        fecha:"2020-02-11",
-        hora:"09:00",
-    },
-    {
-        especialidad:"medicina",
-        nombre:"Dr. Mario",
-        fecha:"2021-05-01",
-        hora:"15:00",
-    },
-    {
-        especialidad:"medicina",
-        nombre:"Dr. Mario",
-        fecha:"2021-03-15",
-        hora:"15:00",
-    },
-    {
-        especialidad:"medicina",
-        nombre:"Dr. Mario",
-        fecha:"2015-01-01",
-        hora:"15:00",
-    },
-    {
-        especialidad:"optometria",
-        nombre:"Dr. Mario",
-        fecha:"2020-01-01",
-        hora:"16:00",
-    }
-]  */
-//console.info(CITAS);
+const obtenerCitasDisponibles = async (especialidad,fecha_inicio,fecha_final) => {
 
-   
-    const obtenerCitasDisponibles = (especialidad,fecha_inicio, fecha_final) =>{
+    let response = await fetch("https://misiontic2022upb.vercel.app/api/medical-appointments/appointments");
+    let citas = await response.json();
     formatFechaInicio = Date.parse(fecha_inicio);
     formatFechaFinal = Date.parse(fecha_final);
-    return CITAS.filter(cita => cita.especialidad==especialidad & Date.parse(cita.fecha)>=formatFechaInicio & Date.parse(cita.fecha)<=formatFechaFinal);
-    }
-
-//console.info(obtenerCitasDisponibles("medicina", "2014-12-31", "2021-07-07"));
-
-
-    const obtenerCitasPorJornada=(especialidad, fecha_inicio, fecha_final, jornadaPreferida)=>{
-                        
-        const horaInicio = new Date();
-        const horaFin = new Date();
-
-        if (jornadaPreferida === "mañana") { 
-            horaInicio.setHours("08","00");
-            horaFin.setHours("12","00");
-        } else if (jornadaPreferida === "tarde"){
-            horaInicio.setHours("13","00");
-            horaFin.setHours("17","00");
-        }
-        
-        let resultados = obtenerCitasDisponibles (especialidad, fecha_inicio, fecha_final);
-
-        return resultados.filter(function (resultado) {
-            let horaDiv = resultado.hora.split(":");
-            const citaFinal = new Date;
-            citaFinal2 = citaFinal.setHours(horaDiv[0],horaDiv[1]);
-            return citaFinal2 >= horaInicio & citaFinal2 <= horaFin;
-        });      
-    }
-
-//console.info(obtenerCitasPorJornada("medicina", "2019-12-31", "2020-02-12", "mañana"));
+    return citas.filter(cita => cita.especialidad==especialidad & Date.parse(cita.fecha)>=formatFechaInicio & Date.parse(cita.fecha)<=formatFechaFinal);
+    
+};
+console.info(obtenerCitasDisponibles("odontología", "2020-01-01", "2021-06-06"));
 
 
-module.exports.obtenerCitasDisponibles=obtenerCitasDisponibles;
-module.exports.obtenerCitasPorJornada=obtenerCitasPorJornada;
+//module.exports.obtenerCitasDisponibles=obtenerCitasDisponibles;
+//module.exports.confirmarCita=confirmarCita;
